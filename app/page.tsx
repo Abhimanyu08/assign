ba014d99d6ea36bf27998a4dbf5819385524f10e
Abhimanyu <iamabhimanyu08@gmail.com>
2023-08-13 14:31:55 +0530

    hell

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,172 @@
-import Image from 'next/image'
+"use client";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { LeafTag, NodeTag } from "./types";
+import { AiOutlineDown, AiOutlineRight } from "react-icons/ai";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export const rootTag: NodeTag = {
+	name: "root",
+	children: [
+		{
+			name: "child1",
+			children: [
+				{ name: "child1-child1", data: "c1-c1 Hello" },
+				{ name: "child1-child2", data: "c1-c2 JS" },
+			],
+		},
+		{ name: "child2", data: "c2 World" },
+	],
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+function Home() {
+	const [root, setRoot] = useState(rootTag);
+	const [rootJson, setRootJson] = useState("");
+	useEffect(() => {
+		if (!rootJson) return;
+		setRootJson(JSON.stringify(root, null, 2));
+	}, [root]);
+	return (
+		<div className="px-10">
+			<NodeTagComponent tag={root} {...{ root, setRoot }} />
+			<Button onClick={() => setRootJson(JSON.stringify(root, null, 2))}>
+				Export
+			</Button>
+			{rootJson && <p>{rootJson}</p>}
+		</div>
+	);
 }
+
+export function LeafTagComponent({
+	tag,
+	root,
+	setRoot,
+}: {
+	tag: LeafTag;
+	root: NodeTag;
+	setRoot: Dispatch<SetStateAction<NodeTag>>;
+}) {
+	return (
+		<TagRenderer {...{ tag, root, setRoot }}>
+			<div className="flex bg-gray-200 items-center gap-2 p-2">
+				<span>Data</span>
+				<input
+					type="text"
+					value={tag.data}
+					className="text-black border-[1px] border-black p-1"
+					onChange={(e) => {
+						tag.data = e.currentTarget.value;
+						let newRoot = structuredClone(root);
+						setRoot(newRoot);
+					}}
+				/>
+			</div>
+		</TagRenderer>
+	);
+}
+
+export function NodeTagComponent({
+	tag,
+	root,
+	setRoot,
+}: {
+	tag: NodeTag;
+	root: NodeTag;
+	setRoot: Dispatch<SetStateAction<NodeTag>>;
+}) {
+	return (
+		<TagRenderer {...{ tag, root, setRoot }}>
+			{tag.children.map((c) => {
+				if (Object.hasOwn(c, "children")) {
+					return (
+						<NodeTagComponent
+							tag={c as NodeTag}
+							{...{ root, setRoot }}
+						/>
+					);
+				}
+
+				return (
+					<LeafTagComponent
+						tag={c as LeafTag}
+						{...{ root, setRoot }}
+					/>
+				);
+			})}
+		</TagRenderer>
+	);
+}
+
+function TagRenderer({
+	tag,
+	root,
+	setRoot,
+	children,
+}: {
+	tag: NodeTag | LeafTag;
+	root: NodeTag;
+	setRoot: Dispatch<SetStateAction<NodeTag>>;
+
+	children: React.ReactNode;
+}) {
+	const [open, setOpen] = useState(true);
+	const [editTag, setEditTag] = useState(false);
+	const [tagName, setTagName] = useState(tag.name);
+
+	const onAddChild = () => {
+		if (Object.hasOwn(tag, "children")) {
+			(tag as NodeTag).children.push({ name: "New Child", data: "Data" });
+			const newRoot = structuredClone(root);
+			setRoot(newRoot);
+			return;
+		}
+		(tag as any).children = [{ name: "New Child", data: "Data" }];
+		const newRoot = structuredClone(root);
+		setRoot(newRoot);
+	};
+
+	return (
+		<div className={` flex flex-col border-2 border-blue-400 my-2 ml-4`}>
+			<div className="flex justify-between p-2 bg-blue-400">
+				<div className="flex gap-2 items-center">
+					<Button onClick={() => setOpen((p) => !p)}>
+						{open ? <AiOutlineDown /> : <AiOutlineRight />}
+					</Button>
+					{editTag ? (
+						<input
+							type="text"
+							value={tagName}
+							className="text-black border-[1px] border-black p-1"
+							onChange={(e) => {
+								setTagName(e.currentTarget.value);
+							}}
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									tag.name = tagName;
+									setRoot(structuredClone(root));
+									setEditTag(false);
+								}
+							}}
+						/>
+					) : (
+						<span onClick={() => setEditTag(true)}>{tag.name}</span>
+					)}
+				</div>
+				<Button onClick={onAddChild}>Add Child</Button>
+			</div>
+
+			<div className={`${open ? "" : "hidden"}`}>{children}</div>
+		</div>
+	);
+}
+
+function Button({
+	children,
+	...props
+}: React.ComponentPropsWithoutRef<"button">) {
+	return (
+		<button {...props} className="text bg-gray-200 px-2 py-1">
+			{children}
+		</button>
+	);
+}
+
+export default Home;
